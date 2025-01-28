cc444e1923885867baae8b9b4a3f83ac2b19fc40
Barbara <95110916+BarbaraMartel@users.noreply.github.com>
2025-01-28 02:08:27 -0500

    Update pokedex.js

--- assets/js/pokedex.js
+++ assets/js/pokedex.js
@@ -19,7 +19,6 @@ const fetchPokemon = () => {
     if (data) {
 
       let myPokeName = data.name;
-      console.log(myPokeName);
 
       let pokeImg = data.sprites.other["official-artwork"].front_default;
 
@@ -31,53 +30,11 @@ const fetchPokemon = () => {
 
       let pokePeso = data.weight;
 
-      myPokeImg(pokeImg);
-      myPokeType(pokeType);
-      myPokeMoves(pokeMoves);
-      pokemonName(myPokeName);
+      console.log(" ");
 
-      myPokeAltura(pokeAltura);
-      myPokePeso(pokePeso);
-
+      
     }
-  }).catch((error) => {
-    console.log("Error: " + error);
-    alert("Debe ingresar un nombre válido.");
-  });
+  })
+  
 }
 
-const myPokeImg = (url) => {
-  const pokePhoto = document.getElementById("poke-img");
-  pokePhoto.src = url;
-}
-
-const myPokeType = (text) => {
-  const pokemonType = document.getElementById("poke-type");
-  pokemonType.textContent = text;
-}
-
-const myPokeAltura = (text) => {
-  const pokemonAltura = document.getElementById("poke-altura");
-  pokemonAltura.textContent = text;
-}
-
-const myPokePeso = (text) => {
-  const pokemonPeso = document.getElementById("poke-peso");
-  pokemonPeso.textContent = text;
-}
-
-const myPokeMoves = (moves) => {
-  const ulPokeMove = document.getElementById("ulMove");
-  const movesNames = moves.map((item) => item.move.name);
-  ulPokeMove.innerHTML = "";
-
-  movesNames.forEach(item => {
-        pokeMove = ulPokeMove.appendChild(document.createElement('li'));
-        pokeMove.textContent = item;
-      })
-}
-
-const pokemonName = (textName) => {
-  const myPokemonName = document.getElementById("poke-name");
-  myPokemonName.innerHTML = textName;
-}
